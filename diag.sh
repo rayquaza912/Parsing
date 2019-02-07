1933101301d7213b6069c496ddf1aa4b56f5844d
rayquaza912 <antoine.rayquaza@gmail.com>
2019-02-07 23:28:25 +0100

    Execute from slave

---
+++ diag.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+# Dignostic for @Webserver
+# Dependencies : bc, dnsutils, csv2html
+
+#ipv4='192.168.10.10'; dns=carnofluxe.domain
+ipv4=8.8.8.8; dns=google.com
+output=result.csv
+
+function getResponseTime () {
+	for i in `seq 1 3`; do
+		eval seq_${i}=`ping -c 1 $1 | head -n 2 | tail -n 1 | cut -d ' ' -f 7 | grep -oE '[0-9]+\.[0-9]+'`
+	done
+
+	if [ -z $seq_1 ] || [ -z $seq_2 ] || [ -z $seq_3 ]; then
+		echo 'Error'
+	else
+		r=$(echo "$seq_1+$seq_2+$seq_3" | bc )
+		#echo "Average response time : $r ms"
+		echo "$r ms"
+	fi
+}
+
+function getResolvStat () {
+	a=`nslookup $1 | grep -oE 'name = ([a-z0-9]|\-|\.)+' | sed 's/name = //g'`
+	b=`nslookup google.com | grep -oE 'Address: ([0-9]{1,3}\.){3}[0-9]{1,3}' | sed 's/Address: //g'`
+
+	if [ "$a" = "" ] || [ "$b" = "" ]; then
+		echo 'Error'
+	else
+		echo "${a},${b}"
+	fi
+}
+
+function checkVirtualHosts () {
+	list=""
+	httpStatus=""
+	for i in $@; do
+		if wget --spider $i 2> /dev/null; then
+			#echo "$i is up !"
+			eval httpStatus="${httpStatus},UP"
+		else
+			#echo "$i is down."
+			eval httpStatus="${httpStatus},DOWN"
+		fi
+		eval list="${list},${i}"
+	done
+
+	echo $httpStatus
+	if [ ! -f $output ]; then
+		echo "Timestamp,Average response time,Name resolution, Reverse resolution${list}" > $output
+	fi
+}
+
+tStamp=$(date +%x-%X)
+rTime=`getResponseTime $ipv4`
+rStatus=`getResolvStat $ipv4 $dns`
+vHosts=`checkVirtualHosts carnofluxe.domain supervision.carnofluxe.domain wiki.carnofluxe.domain google.com`
+
+echo "${tStamp},${rTime},${rStatus}${vHosts}" >> $output
+

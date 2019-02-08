79edd5931b5b40cd38303add971f05bb99aed55b
rayquaza912 <antoine.rayquaza@gmail.com>
2019-02-08 23:43:36 +0100

    Add files via upload

--- backup.sh
+++ backup.sh
@@ -4,7 +4,14 @@
 directories='/var/www/ /etc/apache2/ /var/log/apache2/'
 destination='/mnt/raid1'
 list='~/last_backup.list'
+checkfile='errors.tmp'
 date=`date +%d-%m-%Y`
 
-tar cvzf ${destination}/backup-${date}.tar.gz --listed-incremental=${list} $directories
+tar cvzf ${destination}/backup-${date}.tar.gz --listed-incremental=${list} $directories 2>  $checkfile
+cp $list ${destination}/list-${date}.list
 
+if [ ! -z `cat $checkfile` ]; then
+	# notify admin
+else
+	rm $checkfile
+fi

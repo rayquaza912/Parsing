7c2c0c9994674661f1b5a9fd4ef255ac7b53eb24
rayquaza912 <antoine.rayquaza@gmail.com>
2019-02-11 09:21:38 +0100

    Improved date & time format, makeBakup() function

---
+++ backup_2.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+# Apache2 Data backups
+
+directories='/var/www/ /etc/apache2/ /var/log/apache2/'
+destination='/mnt/raid1'
+list='~/last_backup.list'
+checkfile='~/backup_errors.tmp'; checksize=`cat $checkfile | wc -l`
+date=`date +%d-%m-%Y_%H_%M`
+
+function makeBackup () {
+
+	mkdir ${destination}/${date}/
+	if [ "$1" = "inc" ]; then
+		tar cvzf ${destination}/${date}/backup-${date}.tar.gz --listed-incremental=${list} $directories 2>> $checkfile
+		cp $list ${destination}/${date}/list-${date}.list
+		cp $checkfile ${destination}/${date}/errors-${date}.tmp
+	else
+		tar cvzf ${destination}/${date}/backup-${date}.tar.gz $directories 2>> $checkfile
+		cp $checkfile ${destination}/${date}/errors-${date}.tmp
+	fi
+	warnAlert
+
+}
+
+function warnAlert() {
+
+if [ `cat $checkfile | wc -l` -gt $checksize ]; then
+	echo '============================'; echo -e "\033[33mWarning : \e[0m please see $checkfile for more details."
+	from=`echo "$(cat $checkfile | wc -l) - $cheksize + 1" | bc`
+	tail -n $from $chekfile
+	echo '============================'
+	# notify admin
+fi
+
+}
+
+function checkDate () {
+
+currentTime=`date +%s`
+toDelete=''
+for i in `ls`; do
+	creationTime=`date -d $(stat -c %y $i | cut -d ' ' -f 1) +%s`
+	gapTime=`echo "$currentTime - $creationTime" | bc`
+
+	if [ $gapTime -ge 15814800 ]; then
+	echo '============================'; echo -e "\033[33mWarning : \e[0m some files no longer requiered :"
+	eval toDelete="$toDelete, $i"
+	fi
+done
+echo $toDelete
+echo '============================'
+
+}
+
+makeBackup $1
+
+if [ `ls $destination | head | wc -l` -ge 180 ]; then
+	checkDate
+fi
